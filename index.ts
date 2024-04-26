db3ba029d6c755f6d17f4b947fa1a39de9198feb
farzamyousuf202 <90801520+farzamyousuf202@users.noreply.github.com>
2024-04-27 00:03:43 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env Node
+import inquirer from "inquirer";
+
+let  para = await  inquirer.prompt([
+    {
+        name: "paragraph",
+        type: "string",
+        message: "Enter Your Paragraph"
+    }
+])
+let answer = para.paragraph.trim().split(" ")
+console.log(answer);
+console.log(answer.length);
